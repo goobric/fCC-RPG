58170c20e9782dbace8b11ce7cccd90cdd19dfb6
mlaane <goobric@gmail.com>
2023-12-21 11:25:37 +0000

    js file

---
+++ script.js
@@ -0,0 +1,18 @@
+var xp = 0;
+var health = 100;
+var gold = 50;
+var currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ['stick', 'dagger', 'sword'];
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+
+const text = document.querySelector('#text');
+const xpText = document.querySelector('#xpText');
+const healthText = document.querySelector('#healthText');
+const goldText = document.querySelector('#goldText');
+const monsterStats = document.querySelector('#monsterStats');
+const monsterName = document.querySelector('#monsterName');
